342f2197a90630bbe2e3af08b2e132cb602866f7
Manav <trivedimanav2003@gmail.com>
2026-07-11 19:11:22 +1000

    Finished client code

--- client/main.go
+++ client/main.go
@@ -19,4 +19,28 @@ func main() {
 	defer conn.Close()
 
 	client := userpb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Create a new user
+	createReq := &userpb.CreateUserRequest{
+		Name: "John Doe",
+		Email: "john.doe@example.com",
+	}
+	newUser, err := client.CreateUser(ctx, createReq)
+	if err != nil {
+		log.Fatalf("could not create user: %v", err)
+	}
+	log.Printf("Created user: %v", newUser)
+
+	// Fetch the user we just created 
+	getReq := &userpb.GetUserRequest{
+		Id: newUser.Id,
+	}
+	fetchedUser, err := client.GetUser(ctx, getReq)
+	if err != nil {
+		log.Fatalf("could not get user: %v", err)
+	}
+	log.Printf("Fetched user: %v", fetchedUser)
 }
