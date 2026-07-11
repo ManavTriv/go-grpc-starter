cfb5c4d5ebf355809f8c26eaa742a91782994730
Manav <trivedimanav2003@gmail.com>
2026-07-11 18:52:15 +1000

    Added basic client code

---
+++ client/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	userpb "go-grpc-starter/gen/userpb"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := userpb.NewUserServiceClient(conn)
+}
