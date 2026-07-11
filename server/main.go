02097ca61660315f57111165faee0a3e938d4969
Manav <trivedimanav2003@gmail.com>
2026-07-11 18:20:54 +1000

    Added more basic setup for server

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"fmt"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -15,4 +16,23 @@ import (
 type server struct {
 	userpb.UnimplementedUserServiceServer
 	users map[string]*userpb.User
+}
+
+func (s *server) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.User, error) {
+	user, exists := s.users[req.Id]
+	if !exists {
+		return nil, status.Errorf(codes.NotFound, "user with id %s not found", req.Id)
+	}
+	return user, nil
+}
+
+func (s *server) CreateUser(ctx context.Context, req *userpb.CreateUserRequest) (*userpb.User, error) {
+	id := fmt.Sprintf("user-%d", len(s.users)+1)
+	user := &userpb.User{
+		Id: id,
+		Name: req.Name,
+		Email: req.Email,
+	}
+	s.users[id] = user
+	return user, nil
 }
